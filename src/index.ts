ffe6cd3c1732d7cd8c108430b9b9edd78f6ffb4a
Renata Abukhovich <mrfreetsonn@yandex.by>
2024-09-10 18:14:13 +0300

    documentation first

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
 import './scss/styles.scss';
 
+// типы
+type orderUserData = Pick<IOrder, 'email' | 'phone'>;
+type orderDelivery = Pick<IOrder, 'payment' | 'address'>; 
+type itemCategoryType = | 'софт-скил' | 'другое' | 'дополнительное' | 'кнопка' | 'хард-скил';
+
+// юзер-апи
+export interface IUserAPI {
+    getItemsList(): Promise<IProduct[]>;
+    makeOrder(order: IOrder): Promise<IOrder>;
+  }
+
+  // интерфейс для товара
+export interface IProduct {
+	id: string;
+	description?: string;
+	image: string;
+	name: string;
+	category: string;
+	price: number | null;
+}
+
+// интерфейс для корзины
+export interface IBasket {
+	items: IProduct[];
+	total: number; 
+}
+
+// интерфейс для заказа
+export interface IOrder {
+	address: string;
+	phone: string;
+	payment: string;
+	email: string;
+	total: number;
+	items: string[]
+}
+
+// интерфейс проверки формы
+export interface IFormIsValid {
+    valid: boolean;
+    errors: string[];
+}
+
+// интерфейс для результатов заказа
+export interface IOrderResult {
+    id: string;
+    status: 'success' | 'error';
+    message?: string;
+  }
